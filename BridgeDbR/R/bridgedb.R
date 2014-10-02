1cab96f82be0d7cc54eb42592e3722c0c9a08d10
Anwesha <pennatula@gmail.com>
2014-10-02 16:42:10 +0200

    Passes BiocCheck
    Conflicts:
    	BridgeDbR/DESCRIPTION
    	BridgeDbR/NAMESPACE
    	README.md

--- BridgeDbR/R/bridgedb.R
+++ BridgeDbR/R/bridgedb.R
@@ -1,28 +1,19 @@
-# Copyright (C) 2011  Christ Leemans <christleemans@gmail.com>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details. 
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-.packageName <- "bridgedbr"
+.packageName <- "BridgeDbR"
 
 require(rJava, quietly=TRUE)
-require(bridgedbrlibs, quietly=TRUE)
 
 .onLoad <- function(lib, pkg) {
     dlp<-Sys.getenv("DYLD_LIBRARY_PATH")
     if (dlp!="") { # for Mac OS X we need to remove X11 from lib-path
         Sys.setenv("DYLD_LIBRARY_PATH"=sub("/usr/X11R6/lib","",dlp))
     }
+	libJars = c(
+        "org.bridgedb.jar",
+        "org.bridgedb.bio.jar",
+        "org.bridgedb.rdb.jar",
+        "derby.jar"
+    	)
+    .jpackage(pkg, jars=libJars)
 #make sure the biodatasources are instantiated
 .jcall("org/bridgedb/bio/BioDataSource","V","init")
 }
