8136baf1a567c20a1d3356bc01a12fbb319deebb
Egon Willighagen <egon.willighagen@gmail.com>
2015-08-23 20:37:42 +0200

    Fixed the test call

--- BridgeDbR/tests/testthat/test-getOrganismCode.R
+++ BridgeDbR/tests/testthat/test-getOrganismCode.R
@@ -5,6 +5,6 @@ test_that("it returns an Organism code", {
     expect_equal("Hs", getOrganismCode("Homo sapiens"))
 })
 
-test_that("it gives an error for 'Unknown organism'", {
-    expect_error("Unknown species: Foo bar", getOrganismCode("Foo bar"))
+test_that("it gives an error for 'Foo'", {
+    expect_that(getOrganismCode("Foo"), throws_error("Unknown species: Foo"))
 })
