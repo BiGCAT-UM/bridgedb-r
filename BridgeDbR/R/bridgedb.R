30cc41e78a868a3205c11765e44bd24f2cd80b51
Egon Willighagen <egon.willighagen@gmail.com>
2015-08-23 18:50:12 +0200

    Ported a patch by Anwesha: https://github.com/pennatula/bridgedb-r/commit/ebce8ebab22de772ebeb29d2ba04139c9cf611c6

--- BridgeDbR/R/bridgedb.R
+++ BridgeDbR/R/bridgedb.R
@@ -4,10 +4,11 @@ require(rJava, quietly=TRUE)
 
 .onLoad <- function(lib, pkg) {
     dlp<-Sys.getenv("DYLD_LIBRARY_PATH")
-    if (dlp!="") { # for Mac OS X we need to remove X11 from lib-path
+    if (Sys.info()['sysname'] == "Darwin") 
+    {
         Sys.setenv("DYLD_LIBRARY_PATH"=sub("/usr/X11R6/lib","",dlp))
     }
-	libJars = c(
+    libJars = c(
         "org.bridgedb.jar",
         "org.bridgedb.bio.jar",
         "org.bridgedb.rdb.jar",
